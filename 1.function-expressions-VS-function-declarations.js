05a325bcd2ce823e97e7db381a034fb8f8b17b7f
Phil <happy.melodist@gmail.com>
2021-12-16 01:13:43 -0600

    review the difference between function declarations and function expressions

--- 1.function-expressions-VS-function-declarations.js
+++ 1.function-expressions-VS-function-declarations.js
@@ -1,4 +1,8 @@
+// the following is a function declaration
+
 function myMovieRating() {}
+
+// the following is a function expression
 
 var yourMovieRating = function anotherRating() {
   console.log(anotherRating);
@@ -7,3 +11,5 @@ var yourMovieRating = function anotherRating() {
 console.log(myMovieRating);
 console.log(yourMovieRating);
 console.log(anotherRating);
+
+// at long last, the answer(s) to your overwhelming question -- the differences between function expressions and function declarations
